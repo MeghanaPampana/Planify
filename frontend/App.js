ad7149aa88287098cbcbd6b3759599ee6083aca8
Meghana <112173242+MeghanaPampana@users.noreply.github.com>
2024-06-29 22:31:56 +0530

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -4,10 +4,8 @@ import Register from './components/Register';
 import Home from './components/Home';
 import Login from './components/Login';
 import Main from './components/Main';
-import Dashboard from './components/Dashboard';
 import Projects from './components/Projects';
-import Calendar from './components/Calender';
-import Updates from './components/Updates';
+import Calendar from './components/Calendar';
 import Profile from './components/Profile';
 
 function App() {
@@ -18,10 +16,8 @@ function App() {
         <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Register/>} />
         <Route path="/main" element={<Main/>} />
-        <Route path="/dashboard" element={<Main />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/calendar" element={<Calendar />} />
-        <Route path="/updates" element={<Updates />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
